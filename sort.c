34a1f0f5741a5ebf74316e14d3de9c5c62bda80c
Fernando Contreras <luisfernando_contreras@hotmail.com>
2019-04-06 03:28:45 -0600

    sortEnhanced

--- sort.c
+++ sort.c
@@ -1,41 +1,43 @@
+// Este algoritmo permite ordenar de menor a mayor los elementos de una matriz
 #include<stdio.h>
 #include<stdbool.h>
 
-bool test(int num, int array[4]);
+bool test(int num, int array[1], int size);
 
 int main() {
-    int min, max, min2 = 0, max2, countArray = 0;
-    int array[4] = {10,11,10,1};
-    int array2[4];
-    int array3[4];
+    int min, max, max2, countArray = 0;
+    int array[10] = {4, 4, 6, 1, 0, 3, 6, 9, 9, 0};
+    int array2[10];
+    int array3[10];
+    int size = size = sizeof(array)/sizeof(int);
 
     printf("Matriz inicial: ");
-    for (int count = 0; count < sizeof(array)/sizeof(int); count ++){
+    for (int count = 0; count < size; count ++){
         printf("%d ", array[count]);
     }
 
     // Este for es para obtener el valor mas alto en la matriz
     max = array[0];
-    for (int count = 0; count < sizeof(array)/sizeof(int); count ++){
+    for (int count = 0; count < size; count ++){
       if (array[count] > max)
         max = array[count];
     }
 
     // Este for es para obtener el valor mas bajo en la matriz
     min = array[0];
-    for (int count = 0; count < sizeof(array)/sizeof(int); count ++){
+    for (int count = 0; count < size; count ++){
       if (array[count] < min)
         min = array[count];
     }
 
     // Este for es para ordenar los numeros dentro de la matriz
-    for (int count = 0; count < sizeof(array)/sizeof(int); count++){
+    for (int count = 0; count < size; count++){
         max2 = max;
-        for (int count2 = 0; count2 < sizeof(array)/sizeof(int); count2++){
-            if (test(count2, array3) == true){
-                if ((array[count2] - min) < max2){
+        for (int count2 = 0; count2 < size; count2++){
+            if (test(count2, array3, size) == true){
+                if ((array[count2] - min) <= max2){
                   array2[countArray] = array[count2];
-                  max2 = array[count2];
+                  max2 = array[count2] - min;
                   array3[countArray] = count2;
                 }
             }
@@ -53,8 +55,8 @@ int main() {
 }
 
 // Este for es para evaluar si un numero ya fue alamacenado en la nueva matriz
-bool test(int num, int array3[4]){
-  for(int count = 0; count < 4; count++){
+bool test(int num, int array3[1], int size){
+  for(int count = 0; count < size; count++){
       if (num == array3[count])
         return false;
   }
